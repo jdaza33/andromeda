5903bd9c6fcde485485912320087cef9d1901d37
J. Daza <blackencio33@gmail.com>
2018-06-20 14:24:45 -0400

    Agregando morgan npm

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 
 const express = require('express')
 const nodemon = require('nodemon')
+const morgan = require('morgan')
 
 const app = express()
 
 //Settings
 app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 
 //Output
